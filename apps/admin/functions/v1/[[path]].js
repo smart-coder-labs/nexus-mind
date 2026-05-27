666ca4af39e1858fda89b1d25f65fe7b822fa7ee
Cesar <cr8297408@gmail.com>
2026-05-26 23:44:11 -0500

    fix(admin): remove Pages Function proxy, point VITE_API_URL to custom domain

--- apps/admin/functions/v1/[[path]].js
+++
@@ -1,6 +0,0 @@
-const BACKEND = 'https://nexusmind-backend.fly.dev';
-
-export async function onRequest({ request }) {
-  const url = new URL(request.url);
-  return fetch(BACKEND + url.pathname + url.search, request);
-}
